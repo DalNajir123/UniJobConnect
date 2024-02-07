c218a015898455dec2cec4ea14f2877232995bbb
Najir <dalnajir123@gmail.com>
2024-02-08 00:27:28 +0530

    update

--- Frontend/src/Slider.jsx
+++ Frontend/src/Slider.jsx
@@ -22,7 +22,7 @@ const Slider = ({children: slide, autoSlide = false, autoSlideInterval = 2000}) 
 
   return (
     <div className='opacity-95 overflow-hidden relative rounded-2xl  border-b-8 border-r-8 border-purple-600 shadow-xl shadow-zinc-800'>
-      <div id='slide' onMouseEnter={hover} className='flex transition-transform ease-out duration-700' style={{transform:`translateX(-${current * 100}%)`}}>
+      <div id='slide'  className='flex transition-transform ease-out duration-700' style={{transform:`translateX(-${current * 100}%)`}}>
         {slide}
       </div>
 
@@ -31,13 +31,17 @@ const Slider = ({children: slide, autoSlide = false, autoSlideInterval = 2000}) 
           <ChevronLeft />
         </button>
         <button onClick={next} className='p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white hover:scale-125  '> 
-          <ChevronRight />
+          <ChevronRight  />
         </button>
       </div>
       <div className='absolute bottom-4 right-4 left-0'>
         <div className='flex items-center justify-center gap-2'>
           {slide.map((s,i)=>(
-            <div key={i} className={`transition-all w-2.5 h-2.5 bg-zinc-800 rounded-full ${current === i ? "px-2.5" : "bg-opacity-50"}`}/>
+            <div 
+              onClick={()=>{
+                setCurrent(i);
+              }}  
+              key={i} className={`transition-all w-2.5 h-2.5 bg-zinc-800 rounded-full ${current === i ? "px-2.5" : "bg-opacity-50"}`}/>
           ))}
         </div>
       </div>
