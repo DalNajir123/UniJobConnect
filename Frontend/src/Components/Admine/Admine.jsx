0bd9ca3e475f684abc6989ac7947a617994d0eb1
Najir <dalnajir123@gmail.com>
2024-02-24 15:12:03 +0530

    test

--- Frontend/src/Components/Admine/Admine.jsx
+++ Frontend/src/Components/Admine/Admine.jsx
@@ -13,6 +13,7 @@ function Admine() {
       <button onClick={toggleDarkmode}>Dark Mode</button>
       <Navbar />
       <div className={darkmode ? 'bg-zinc-900 text-white w-full h-[60vh] flex items-center justify-center' : "w-full h-[60vh] bg-purple-300 flex items-center justify-center text-zinc-800"}>  
+       <h1>NAJIR</h1>
        <Getjob />
       </div>
       <Footer />
