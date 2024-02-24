81f26dd502b3ad655092616a094e1d1b11ad91be
Najir <dalnajir123@gmail.com>
2024-02-24 14:35:43 +0530

    update

--- Frontend/src/Components/Admine/Admine.jsx
+++ Frontend/src/Components/Admine/Admine.jsx
@@ -12,10 +12,9 @@ function Admine() {
     <>
       <button onClick={toggleDarkmode}>Dark Mode</button>
       <Navbar />
-      <div className={darkmode ? 'bg-zinc-900 text-white w-full h-[60vh] flex items-center justify-center' : "w-full h-[60vh] bg-purple-300 flex items-center justify-center text-zinc-800"}>
-        <h1 className="text-3xl font-extrabold ">Admine Home</h1>
+      <div className={darkmode ? 'bg-zinc-900 text-white w-full h-[60vh] flex items-center justify-center' : "w-full h-[60vh] bg-purple-300 flex items-center justify-center text-zinc-800"}>  
+       <Getjob />
       </div>
-      <Getjob />
       <Footer />
     </>
   );
